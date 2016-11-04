a7f4b2c95a1571c77480ba506512a4ebb5de326d
Josh Bowen <jbowen@apigee.com>
2016-11-04 10:54:59 -0600

    Now deletes pods

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # khaos-monkey
+
+Based off ot the ideas from Netflix's [Chaos Monkey](https://github.com/Netflix/chaosmonkey) this tool will randomly delete pods from the kubernetes namespace that it is deployed in.
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
 	"time"
 
 	emoji "gopkg.in/kyokomi/emoji.v1"
@@ -11,7 +13,10 @@ import (
 )
 
 func main() {
-	emoji.Println(":monkey: :poop:")
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	namespace := os.Getenv("MY_POD_NAMESPACE")
+	emoji.Println(":see_no_evil: :hear_no_evil: :speak_no_evil:")
 
 	config, err := rest.InClusterConfig()
 	if err != nil {
@@ -24,11 +29,18 @@ func main() {
 		panic(err.Error())
 	}
 	for {
-		pods, err := clientset.Core().Pods("").List(api.ListOptions{})
+		pods, err := clientset.Core().Pods(namespace).List(api.ListOptions{})
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+		fmt.Printf("There are %d pods in the %s namespace\n", len(pods.Items), namespace)
+		numPods := len(pods.Items)
+		deletingPod := pods.Items[rand.Intn(numPods)].Name
+		tempInt := int64(0)
+		clientset.Core().Pods(namespace).Delete(deletingPod, &api.DeleteOptions{
+			GracePeriodSeconds: &tempInt,
+		})
+		fmt.Printf("Killed Pod: %s\n", deletingPod)
 		time.Sleep(10 * time.Second)
 	}
 
